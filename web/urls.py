548cc76302d9301bc0ccac3405e10a88b4561fdd
Albertojuanse <albertojuansebastian@gmail.com>
2020-04-23 10:52:03 +0200

    metaphore/

--- web/urls.py
+++ web/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url('metaphore/', views.metaphore, name='metaphore'),
 ]
--- web/views.py
+++ web/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def index (request):
     return render(request, 'index.html')
+
+def metaphore (request):
+    return render(request, 'metaphore.html')
